d8008a0b0d525a3df34f2176736d0ea91eb0a928
kaderabdul <kaderabdul7980@gmail.com>
2022-04-11 05:14:22 +0600

    show validation error message

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -17,12 +17,21 @@
                             @csrf
                             <div class="form-group mb-3">
                                 <input type="text" name="name" placeholder="name" class="form-control" id="">
+                                @if ($errors->has('name'))
+                                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                                @endif
                             </div>
                             <div class="form-group mb-3">
                                 <input type="email" name="email" placeholder="email" class="form-control" id="">
+                                @if ($errors->has('email'))
+                                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                                @endif
                             </div>
                             <div class="form-group mb-3">
                                 <input type="password" name="password" placeholder="password" class="form-control" id="">
+                                @if ($errors->has('password'))
+                                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                                @endif
                             </div>
                             <div class="d-grid gap-2">
                               <button type="submit" name="" id="" class="btn btn-dark">Sign Up</button>
